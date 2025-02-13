887323c6227a4047323d4fcbf089766aa10cf58c
Gugu <gugunnn@gmail.com>
2025-02-13 06:28:19 +0200

    Implemented tailwind cli installation

--- configs/vite.config.js
+++ configs/vite.config.js
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite';
-import { createHtmlPlugin } from 'vite-plugin-html';
+import postcss from 'postcss';
 
 export default defineConfig({
-  plugins: [
-    createHtmlPlugin({
-      inject: true,
-    }),
-  ],
-  server: {
-    watch: {
-      usePolling: true,
-      interval: 100,
-      ignored: ['**/*.js', 'dist', 'node_modules'],
-    },
-  },
+  css: {
+    postcss
+  }
 });
