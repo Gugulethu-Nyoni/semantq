d95bbcfed2dae0ad5fe36f4771f45a981a589b22
Gugu <gugunnn@gmail.com>
2025-02-23 09:02:04 +0200

    Finalised cli for supabase and mscr set up

--- server/controllers/index.js
+++
@@ -1,4 +0,0 @@
-export { default as UserController } from './UserController.js';
-export { default as TicketController } from './TicketController.js';
-export { default as CompanyController } from './CompanyController.js';
-export { default as EntityController } from './EntityController.js';
