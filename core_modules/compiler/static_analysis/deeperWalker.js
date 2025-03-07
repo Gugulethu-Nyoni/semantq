c394d2b9f55e7212214b51f0f3668e3f8dbe4385
Gugu <gugunnn@gmail.com>
2025-03-07 08:36:42 +0200

    Updated getNodePositions to check for nodes not only in children but for attributes as well - so now get node positions can get attribute node locations

--- core_modules/compiler/static_analysis/deeperWalker.js
+++ core_modules/compiler/static_analysis/deeperWalker.js
@@ -99,32 +99,7 @@ walk(ast, targetNodeType) {
 
 
 
-// Helper function for deep equality check
-/*
-deepEqual(a, b) {
-  const walk = new Walker();
 
-  if (a === b) return true;
-  if (a == null || b == null) return false;
-  if (Array.isArray(a) && Array.isArray(b)) {
-    if (a.length !== b.length) return false;
-    for (let i = 0; i < a.length; i++) {
-      if (!walk.deepEqual(a[i], b[i])) return false;
-    }
-    return true;
-  }
-  if (typeof a === 'object' && typeof b === 'object') {
-    const keysA = Object.keys(a);
-    const keysB = Object.keys(b);
-    if (keysA.length !== keysB.length) return false;
-    for (const key of keysA) {
-      if (!keysB.includes(key) || !walk.deepEqual(a[key], b[key])) return false;
-    }
-    return true;
-  }
-  return false;
-}
-*/
 
 deepWalker(ast, nodeType = null, matchLogic, returnType = null, pathValue = null) {
     if (!ast) {
