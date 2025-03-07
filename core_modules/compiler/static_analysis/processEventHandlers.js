efd47d0b817e49ade0ffaa2222b6e9196931a6c8
Gugu <gugunnn@gmail.com>
2025-03-07 10:09:41 +0200

    Updated event handler processing for handling multiple events on a single node efficiently

--- core_modules/compiler/static_analysis/processEventHandlers.js
+++ core_modules/compiler/static_analysis/processEventHandlers.js
@@ -1,68 +1,91 @@
 import EventHandlerProcessor from './transformEventHandlers.js';
 import GetNodePositions from '../utils/GetNodePositions.js';
 import Walker from './deeperWalker.js';
+import { _generateUniqueElementId} from '../utils/utils.js';
+import { parse } from 'acorn';
 
 
 
-export default class ProcessEventHandlers {       
+export default class ProcessEventHandlers {
+  constructor(customAST, jsAST) {
+    this.customAST = customAST;
+    this.jsAST = jsAST;
+    this.idCache = new Map(); // Cache to store element IDs
+    this.process();
+  }
 
-constructor (customAST, jsAST) {
-this.customAST=customAST;
-this.jsAST=jsAST;
-this.process();
+  process() {
+    const walk = new Walker();
+    const nodeType = 'Attribute';
+    const returnType = { path: 'value[0].type' };
+    const pathValue = 'MustacheAttribute';
+    const matchLogic = walk.createMatchLogic(nodeType);
 
-}
+    const eventHandlers = walk.deepWalker(
+      this.customAST,
+      nodeType,
+      matchLogic,
+      returnType,
+      pathValue
+    );
+
+    eventHandlers.forEach(attr => {
+      const eventName = attr.node.name.name;
+      const eventFunctionName = attr.node.value[0].name.name;
+
+      // Get node locations
+      const getNodeLocations = new GetNodePositions(this.customAST, attr.node);
+      const nodeLocations = getNodeLocations.init();
+      const parentNodeandIndex = nodeLocations[0];
+
+      // Check if the element already has an ID in the cache
+      const elementKey = `${parentNodeandIndex.parentNode.start}-${parentNodeandIndex.parentNode.end}`;
+      let elementId = this.idCache.get(elementKey);
+
+      if (!elementId) {
+        // Generate a new ID and cache it
+        elementId = _generateUniqueElementId();
+        this.idCache.set(elementKey, elementId);
+
+        // Add the ID to the AST
+        this.addIdToAST(parentNodeandIndex.parentNode, elementId);
+      }
+
+      const eventListenerCodeBuilder = new EventHandlerProcessor(
+        attr.node,
+        eventName,
+        eventFunctionName,
+        pathValue,
+        parentNodeandIndex,
+        elementId // Pass the cached ID
+      );
+
+      const eventListenerCode= eventListenerCodeBuilder.process();
+      const eventListenerAST = parse(eventListenerCode, { ecmaVersion: 'latest' });
+      this.jsAST.body.push(...eventListenerAST.body);
+
+      //console.log(this.jsAST);
 
 
+    });
+  }
 
-         process () {
+  addIdToAST(parentNode, elementId) {
+    parentNode.attributes.unshift({
+      start: 150,
+      end: 164,
+      type: "Attribute",
+      name: "id",
+      value: [{
+        start: 154,
+        end: 163,
+        type: "Text",
+        raw: elementId,
+        data: elementId,
+      }],
+    });
+  }
 
-            //console.log(this.customAST, this.jsAST)
-
-            const walk = new Walker();
-
-
-          
-            let eventHandlers;
-            let nodeType = 'Attribute';
-            let returnType = { path:'value[0].type' };
-            let pathValue = 'MustacheAttribute';
-            let matchLogic = walk.createMatchLogic(nodeType);
-            eventHandlers = walk.deepWalker(this.customAST, nodeType, matchLogic, returnType,pathValue);
-
-            //console.log(JSON.stringify(eventHandlers,null,2));
-
-            eventHandlers.forEach(attr => {
-
-                const eventName = attr.node.name.name;
-                const eventFunctionName = attr.node.value[0].name.name;
-
-                // get node locations
-
-                const getNodeLocations = new GetNodePositions(this.customAST, attr.node);
-                const nodeLocations = getNodeLocations.init();
-                const parentNode = nodeLocations[0].parentNode;
-                const parentNodeandIndex=nodeLocations[0]; 
-
-                //console.log(parentNode);
-
-
-
-
-        const eventListenerCodeBuilder = new EventHandlerProcessor(attr.node, eventName, eventFunctionName, pathValue, parentNodeandIndex);
-        const eventListenerCode = eventListenerCodeBuilder.process();
-
-
-            console.log(eventListenerCode);
-
-
-
-
-            }); 
-
-            
-            
-}
 
 
 
