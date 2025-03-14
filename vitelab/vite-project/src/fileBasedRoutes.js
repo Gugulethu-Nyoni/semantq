72e585975ee585a73ce0b7370e3bd12b6055738f
Gugu <gugunnn@gmail.com>
2025-03-14 12:46:56 +0200

    Overhauled the router for bundled asset handling

---
+++ vitelab/vite-project/src/fileBasedRoutes.js
@@ -0,0 +1,10 @@
+const fileBasedRoutes = {
+  "/": "/",
+  "home": "/",
+  "about": "src/routes/about",
+  "services": "src/routes/services",
+  "services/asia": "src/routes/services/asia",
+  "services/asia/east": "/src/routes/services/asia/east"
+};
+
+export default fileBasedRoutes;
---
+++ vitelab/vite-project/src/routes/services/asia/east/east.js
@@ -0,0 +1,1 @@
+console.log('Hello from the East Asia Services page!');
