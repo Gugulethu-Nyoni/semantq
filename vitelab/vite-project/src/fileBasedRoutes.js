843f8361bff0c1a7719367e52c784244cfa42012
Gugu <gugunnn@gmail.com>
2025-03-14 14:12:58 +0200

    Cleaned up router logs and alerts

--- vitelab/vite-project/src/fileBasedRoutes.js
+++ vitelab/vite-project/src/fileBasedRoutes.js
@@ -2,6 +2,7 @@ const fileBasedRoutes = {
   "/": "/",
   "home": "/",
   "about": "src/routes/about",
+  "store": "src/routes/store",
   "services": "src/routes/services",
   "services/asia": "src/routes/services/asia",
   "services/asia/east": "/src/routes/services/asia/east"
