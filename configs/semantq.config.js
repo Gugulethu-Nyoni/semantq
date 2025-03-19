cca5d50eef0546cdd6b6e908643dff62a2a77c38
Gugu <gugunnn@gmail.com>
2025-03-19 06:06:54 +0200

    Added semantq config to the configs dir

--- configs/semantq.config.js
+++ configs/semantq.config.js
@@ -2,6 +2,11 @@ import path from 'path';
 const rootDir = process.cwd();
 
 export default {
+  domain: process.env.DOMAIN || 'localhost', // Local development domain
+  targetHost: process.env.TARGET_HOST || 'https://example.com', // Live production domain
+  pageTitle: 'My Awesome Website', // Default page title
+  envFilePath: path.join(rootDir, '.env'), // Path to the .env file in the root of the project
+
   routes: {
     declaredRoutes: path.join(rootDir, 'build/routes/routes.js'),
     fileBasedRoutes: path.join(rootDir, 'build/routes/fileBasedRoutes.js'),
