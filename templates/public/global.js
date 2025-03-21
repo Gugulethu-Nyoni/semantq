1c50521998c2d4af4a9fa187e77ad47ffb251930
Gugu <gugunnn@gmail.com>
2025-03-21 05:21:22 +0200

    Updated the framework's landing page

--- templates/public/global.js
+++ templates/public/global.js
@@ -1,1 +1,9 @@
 console.log("Global JS loaded");
+
+// Simple Counter Logic
+    let count = 0;
+    const counterButton = document.getElementById('counter');
+    counterButton.addEventListener('click', () => {
+      count++;
+      counterButton.textContent = count;
+    });
