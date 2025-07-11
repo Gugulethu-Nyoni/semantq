d9c168f85b07f13b97984b7c538aa748b24ee7e0
Gugu <gugunnn@gmail.com>
2025-07-11 09:55:27 +0200

    Updated router to handle leading slashes form file based routes paths-keys

--- core_modules/compiler/fileBasedRouteGenerator.js
+++ core_modules/compiler/fileBasedRouteGenerator.js
@@ -1,21 +1,29 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+/**
+ * Generate file-based routes for your project
+ * Scans the given basePath and outputs a fileBasedRoutes.js map file
+ */
 export function generateFileBasedRoutes(basePath) {
     const fileBasedRoutes = {};
 
     try {
         traverseDirectory(basePath, '', fileBasedRoutes);
         writeRoutesToFile(basePath, fileBasedRoutes);
+        console.log('File-based routes generated successfully.');
     } catch (err) {
         console.error('Error generating routes:', err);
     }
 }
 
+/**
+ * Recursively traverses directories to build route map
+ */
 function traverseDirectory(directoryPath, relativePath, fileBasedRoutes) {
-    // Add default routes
-    fileBasedRoutes['/'] = '/';
-    fileBasedRoutes['home'] = '/';
+    // Add default routes once
+    if (!fileBasedRoutes['/']) fileBasedRoutes['/'] = '/';
+    if (!fileBasedRoutes['/home']) fileBasedRoutes['/home'] = '/home';
 
     const files = fs.readdirSync(directoryPath);
     files.forEach(file => {
@@ -23,17 +31,35 @@ function traverseDirectory(directoryPath, relativePath, fileBasedRoutes) {
         const stats = fs.statSync(filePath);
 
         if (stats.isDirectory()) {
-            const newRelativePath = `${path.posix.join(relativePath, file)}`;
-            fileBasedRoutes[newRelativePath] = '/' + newRelativePath;
+            const newRelativePath = path.posix.join(relativePath, file);
+
+            // Always prefix with a leading slash
+            const key = path.posix.join('/', newRelativePath);
+
+            fileBasedRoutes[key] = key;
+
+            // Recurse into subdirectory
             traverseDirectory(filePath, newRelativePath, fileBasedRoutes);
         }
     });
 }
 
+/**
+ * Writes the generated routes to fileBasedRoutes.js, sorted alphabetically
+ */
 function writeRoutesToFile(basePath, fileBasedRoutes) {
     const filePath = path.join(basePath, 'fileBasedRoutes.js');
-    //console.log(filePath);
-    const fileContent = `const fileBasedRoutes = ${JSON.stringify(fileBasedRoutes, null, 2)};\n\nexport default fileBasedRoutes;`;
+
+    // Sort routes alphabetically by key for readability
+    const sortedRoutes = Object.keys(fileBasedRoutes)
+        .sort()
+        .reduce((acc, key) => {
+            acc[key] = fileBasedRoutes[key];
+            return acc;
+        }, {});
+
+    const fileContent = `const fileBasedRoutes = ${JSON.stringify(sortedRoutes, null, 2)};\n\nexport default fileBasedRoutes;`;
+
     fs.writeFileSync(filePath, fileContent);
 }
 
