828dcad6250b4ac24a2bcdb37b27d725d27868b7
Gugu <gugunnn@gmail.com>
2025-07-17 07:13:34 +0200

    Updated global css js and entry index.html

--- templates/global.js
+++ templates/global.js
@@ -1,10 +1,4 @@
 import Router from './src/semantq/router.js';
 console.log(Router);
 
-// Simple Counter Logic
-    let count = 0;
-    const counterButton = document.getElementById('counter');
-    counterButton.addEventListener('click', () => {
-      count++;
-      counterButton.textContent = count;
-    });
+console.log('global JS loaded.');
