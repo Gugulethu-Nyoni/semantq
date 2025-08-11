922aa90ea3ede8d57245d37bf357c9a25176da39
Gugu <gugunnn@gmail.com>
2025-08-11 18:29:51 +0200

    updated semantq config js

--- configs/semantq.config.js
+++ configs/semantq.config.js
@@ -1,5 +1,4 @@
 import path from 'path';
-
 const rootDir = process.cwd();
 
 export default {
@@ -9,7 +8,6 @@ export default {
   metaDescription: 'My Awesome Website',
   metaKeywords: 'Awesome this, awesome that',
   envFilePath: path.join(rootDir, '.env'),
-
   sitemap: true,
   base: '/',
 
@@ -29,18 +27,24 @@ export default {
     declaredRoutes: path.join(rootDir, 'build/routes/routes.js'),
     fileBasedRoutes: path.join(rootDir, 'build/routes/fileBasedRoutes.js'),
   },
-
   semantqNav: {
     enable: true,
     containerClass: 'semantq-nav-container',
     ulClass: 'semantq-nav-list',
     liClass: 'semantq-nav-item',
-    excludeRoutes: [],
+    priorityRoutes: ['/home'],
+    excludeRoutes: ['/404'],
+    includeRoutes: {
+      '/sitemap': '/sitemap',
+      '/home': '/'
+    },
     hierarchical: true,
-    parentMenuDisplay: 'stacked',
-    customLinkTexts: {},
-  },
+    parentMenuDisplay: 'inline', // stacked or inline
+    customLinkTexts: {
+      badmin: 'User Dashboard',
 
+    },
+  },
   database: {
     adapter: 'mysql', // can be 'supabase', 'mysql', 'mongodb', 'sqlite'
     config: {
